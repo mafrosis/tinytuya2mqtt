fa7e483c8fef6592e7696f128991347c74481ee3
Matt Black <github@mafro.net>
2022-05-23 15:21:56 +1000

    Add missing package __init__

---
+++ tinytuya2mqtt/__init__.py
@@ -0,0 +1,9 @@
+#
+# tinytuya2mqtt
+#
+
+__title__ = 'tinytuya2mqtt'
+__version__ = '0.1.0'
+__author__ = 'mafrosis'
+__license__ = 'Simplified MIT License'
+__copyright__ = 'Copyright 2022 Matt Black'
